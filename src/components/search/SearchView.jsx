27438e8e15bc71e1e43659e1e464a6b41e0e349c
Quim <quimifrances@gmail.com>
2021-04-14 16:28:03 +0200

    Serch function

--- src/components/search/SearchView.jsx
+++ src/components/search/SearchView.jsx
@@ -1,27 +1,27 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import queryString from 'query-string';
 import { useLocation } from 'react-router';
-import { heroes } from '../../data/heroes'
 import { HeroCard } from '../heroes/HeroCard';
 import { useForm } from '../hooks/useForm';
+import { getHeroesByName } from '../selectors/getHeroesByName';
 
 
 export const SearchView = ({history}) => {
 
     const location = useLocation();
 
-    const{ query = ''} = queryString.parse( location.search);
+    const{ q = ''} = queryString.parse( location.search );
 
     
-    const [formValues, handleInputChange, reset] = useForm( {searchText: query});
+    const [formValues, handleInputChange] = useForm( {searchText: q});
     
     const {searchText} = formValues;
-    
-    const heroesFiltered = heroes;
+
+    const heroesFiltered = useMemo(() => getHeroesByName( q ), [q] );
     
     const handleSearch = (e) => {
         e.preventDefault();
-        history.push(`?q=${searchText}`)
+        history.push(`?q=${searchText}`);
     }
 
     return (
@@ -43,6 +43,19 @@ export const SearchView = ({history}) => {
                 <div className='col-7'>
                     <h4>Results</h4>
                     <hr/>
+
+                    { 
+                        (q === '') && <div className='alert alert-info'>
+                            Search a Hero!
+                        </div>
+                    }
+
+                    {   
+                        (q !== '' && heroesFiltered.length === 0 ) && <div className='alert alert-danger'>
+                            There is no heroes with {q}
+                        </div>
+                    }
+
                     {
                         heroesFiltered.map( hero => (
                             <HeroCard key={hero.id}{...hero}/>
